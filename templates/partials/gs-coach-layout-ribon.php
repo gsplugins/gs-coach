9b31f855895a2da2e543ca9fbd99972d6e35cbef
Mustafiz Munna <munna.fswd@gmail.com>
2026-08-09 15:20:30 +0600

    Fixed ribbon undefined error to archive

--- templates/partials/gs-coach-layout-ribon.php
+++ templates/partials/gs-coach-layout-ribon.php
@@ -5,10 +5,11 @@ namespace GSCOACH;
 if ( $display_ribbon !== 'on' ) return;
 
 $ribbon = get_post_meta( get_the_id(), '_gscoach_ribbon', true );
+$gs_coach_ribbon_style = empty( $gs_coach_ribbon_style ) ? 'default' : $gs_coach_ribbon_style;
 
 if ( !empty($ribbon) ): ?>
 
-    <div class="gs_coach_ribbon gs-coach-ribbon-<?php echo esc_attr($gs_coach_ribbon_style); ?>"><?php echo esc_html( $ribbon ); ?></div>
+    <div class="gs_coach_ribbon gs-coach-ribbon-<?php echo esc_attr( $gs_coach_ribbon_style ); ?>"><?php echo esc_html( $ribbon ); ?></div>
 
     <?php do_action( 'gs_coach_after_coach_ribbon' ); ?>
 <?php endif; ?>
